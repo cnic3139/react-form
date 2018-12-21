29d6675cff8189bc8832059c59a70c335d196bfc
Cameron Nichols <cnic3139@uni.sydney.edu.au>
2018-12-21 12:35:32 +1100

    final commit - 2hrs, 15 mins final time

--- react-form/src/index.js
+++ react-form/src/index.js
@@ -26,26 +26,37 @@ class Form extends React.Component {
 
 	// Get user inputted value out of HTML element.
 	// TODO add case for radio input.
-	getValue(input) {
-		console.log('INPUT:', input);
-		console.log('childNodes:', input.childNodes);
-		const value = input.childNodes[0].childNodes[1];
-		switch(value.type) {
+	getValue(input, type) {
+		let value = input;
+		switch(type) {
 			case 'text':
 			case 'date':
-				return value.value;
-			case 'checkbox':
-				return value.checked;
-			case 'radio':
-				console.log('RADIO INPUT:', input);
-				console.log('RADIO VALUE:', value);
+				return input.childNodes[0].childNodes[1].value;
+			case "radio":
+				value = input.childNodes[0].childNodes;
+				value = Array.prototype.slice.call(value).slice(1, value.length);
+				value = value.map(v => v.childNodes[1]);
+				value = value.map(v => [v.id, v.checked]);
+				return value;
+			case "checkbox":
+				return input.childNodes[0].childNodes[1].childNodes[1].checked;
+			case "array":
+				value = input.childNodes[0].childNodes[1].childNodes;
+				value = Array.prototype.slice.call(value).slice(0, value.length - 1);
+				value = value.map(v => v.childNodes[0].childNodes[1]);
+				value = value.map(v => v.value);
+				return value;
+			case "multipart":
+				value = value.childNodes[0];
+				value = Array.prototype.slice.call(value.childNodes).slice(1, value.length);
+				value = value.map(v => v.childNodes[1]);
+				value = value.map(v => [v.name, v.value]);
 				return value;
 			default:
-					return value;
+				return input;
 		}
 	}
 
-	// TODO
 	// Validate the form agains the 'validations' properties in schema.
 	validate(event) {
 		event.preventDefault();
@@ -54,18 +65,105 @@ class Form extends React.Component {
 
 		const form = document.forms['form'];
 		const formNodes = Array.prototype.slice.call(form.childNodes);
-		this.props.schema.fields.every(field => {
+		valid = this.props.schema.fields.every(field => {
 			const input = formNodes.find(node => node.id === field.id);
 
-			this.getValue(input);
-			return true;
+			// Check field is answered if mandatory
+			if (!field.optional || typeof field.optional === 'object') {
+				let optional;
+
+				// Check type of 'optional' field.
+				if (typeof field.optional === 'object') {
+
+					// Resolve condition in 'optional' object.
+					const dependentOn = formNodes.find(node => node.id === field.optional.dependentOn);
+					const dependentOnValue = this.getValue(dependentOn, field.type);
+
+					// Try and find a value in schema 'values' object that matches a value
+					// given in 'dependentOn' input.
+					// Otherwise, use the default value provided.
+					const value = field.optional.values.find(val => dependentOnValue in val);
+
+					if (value) {
+						optional = value[dependentOnValue];
+					} else {
+						optional = field.optional.default;
+					}
+
+				} else {
+					// Else 'optional' field is just a boolean value.
+					optional = field.optional;
+				}
+
+				// If not optional, make sure input is answered.
+				if (!optional) {
+					return Boolean(this.getValue(input, field.type));
+				}
+			}
+
+			return field.validations.every(validation => {
+				if (validation.containsChar) {
+					// Ensure input value has the specified char.
+					return this.getValue(input, field.type).trim().includes(validation.containsChar);
+
+				} else if (validation.dateGreaterThan) {
+					// Ensure difference between today and given date is larger than specified interval.
+					const value = this.getValue(input, field.type);
+
+					let diff = Date.now() - new Date(value).getTime();
+
+					// Convert from milliseconds to the unit given in schema.
+					switch(validation.dateGreaterThan.unit) {
+						case "seconds":
+							diff = diff / 1000;
+							break;
+						case "minutes":
+							diff = diff / 1000 / 60;
+							break;
+						case "hours":
+							diff = diff / 1000 / 60 / 60;
+							break;
+						case "days":
+							diff = diff / 1000 / 60 / 60 / 24;
+							break;
+						case "years":
+							diff = diff / 1000 / 60 / 60 / 24 / 365;
+							break;
+						default:
+							break;
+
+						return diff > validation.dateGreaterThan.value;
+					}
+				}
+			});
 		});
+
+		// If all checks pass, submit the form. Otherwise alert the user.
+		if (valid) {
+			this.submit();
+		} else {
+			alert("Form is not valid. Please make sure all inputs have been correctly answered.");
+		}
 
 	}
 
 	// Submit the form.
-	// TODO return json object with field data.
 	submit() {
+		let outputData = {};
+		// TODO make these common? i dunno
+		const form = document.forms['form'];
+		const formNodes = Array.prototype.slice.call(form.childNodes);
+		this.props.schema.fields.forEach(field => {
+			const input = formNodes.find(node => node.id === field.id);
+			const inputValue = this.getValue(input, field.type);
+
+			outputData[field.id] = inputValue;
+		});
+
+		// This is the output completed JSON form.
+		console.log("final output: ", outputData);
+
+
 		document.getElementById('form_root').submit();
 	}
 
@@ -91,7 +189,7 @@ class Field extends React.Component {
 			case 'text':
 				return (
 					<label>
-						{value.title}
+						<span>{value.title}:</span>
 						<input type="text" name={value.title} />
 					</label>
 				);
@@ -99,7 +197,7 @@ class Field extends React.Component {
 			case 'date':
 				return (
 					<label>
-						{value.title}
+						<span>{value.title}:</span>
 						<input type="date" name={value.title} />
 					</label>
 				);
@@ -107,7 +205,7 @@ class Field extends React.Component {
 			case 'number':
 				return (
 					<label>
-						{value.title}
+						<span>{value.title}:</span>
 						<input type="number" name={value.title} />
 					</label>
 				);
@@ -115,7 +213,7 @@ class Field extends React.Component {
 			case 'radio':
 				return (
 					<label>
-						{value.title}
+						<span>{value.title}:</span>
 						{value.options.map(option => (
 							<label key={`radio_${option}`}>{option}
 								<input type="radio" id={option} name={value.title} />
@@ -127,7 +225,7 @@ class Field extends React.Component {
 			case 'checkbox':
 				return (
 					<label>
-						{value.title}
+						<span>{value.title}:</span>
 						{value.options.map(option => (
 							<label key={`checkbox_${option}`}>{option}
 								<input type="checkbox" id={option} name={value.title} />
@@ -139,7 +237,7 @@ class Field extends React.Component {
 			case 'multipart':
 				return (
 					<label>
-						{value.title}
+						<span>{value.title}:</span>
 						{value.parts.map(part => (
 							<Field 
 								key={part.id}
@@ -151,13 +249,22 @@ class Field extends React.Component {
 			
 			case 'array':
 				return (
-					// TODO
+					<label>
+						<span>{value.title}:</span>
+						<ArrayField 
+							value={{
+								...value,
+								type: value.arrayOfType,
+								title: null
+							}} 
+						/>
+					</label>
 				);
 			
 			default:
 				return (
 					<label>
-						{value.title}
+						<span>{value.title}:</span>
 						<input type="text" name={value.title} />
 					</label>
 				);
@@ -186,7 +293,21 @@ class ArrayField extends React.Component {
 	}
 
 	render() {
-		// TODO
+		const elements = [...Array(this.state.size)].map((element, index) => (
+			<li key={`li_${this.props.value.id}_${index}`}>
+				<Field 
+					key={this.props.value.id}
+					value={this.props.value}
+				/>
+			</li>
+		));
+
+		return (
+			<ol>
+				{ elements }
+				<button onClick={this.addNewInput}>Add new</button>
+			</ol>
+		);
 	}
 }
 
